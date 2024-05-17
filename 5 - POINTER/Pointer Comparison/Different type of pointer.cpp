5305be9704c59158644d6a7b84d9a593e16124c1
Arman Sani <158436710+ARMANSANISANI@users.noreply.github.com>
2024-05-17 14:25:13 +0530

    Add files via upload

---
+++ 5 - POINTER/Pointer Comparison/Different type of pointer.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int *ptrA;
+    float *ptrB;
+    
+    ptrA = (int *)1000;
+    ptrB = (float *)2000;
+    
+       printf("PtrB is greater than ptrA");
+    
+    return(0);
+}
